7ba185f38b93f413a15208ccde402a9ccb9228d8
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-20 16:04:25 -0400

    changed builddocker
    line 6

--- TestSerial/builddocker.sh
+++ TestSerial/builddocker.sh
@@ -3,5 +3,5 @@
 
 hash=$(git rev-parse --short HEAD)
 docker build --platform linux/arm64 -t ecy14mhfh/testserial:$hash -f Dockerfile .
-docker push rianders/car:$hash
+docker push ecy14mhfh/testserial:$hash
 echo $hash
