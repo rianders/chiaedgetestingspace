7d416e44eaea2e30d68c2397cb555d2bac2e4b5b
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-20 14:22:51 -0400

    initial document check in

---
+++ TestSerial/TestSerial.py
@@ -0,0 +1,20 @@
+# Read a line of serial data from a serial port and print it to the screen
+
+import serial
+import time
+
+# ports for seiral communication: 
+PORT_NAME = '/dev/ttyUSB0'
+# check which port is being used in pio device monitor V
+
+with  serial.Serial( PORT_NAME,  9600, timeout=1) as ser:
+    print(f"Start Serial: name: {ser.name}.  ")
+    time.sleep(1)
+    for i in range(50):
+        print(f"line: {i}")
+        #if(ser.in_waiting > 0):
+        line = ser.readline()
+        print(line.decode('utf-8'))
+
+ser.close()
+print("Stop Serial: ")
