14beab30201e2b78faefdd8540cc966f54790a3c
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-21 11:07:20 -0400

    Fixed file name issue
    #4

--- TestSerial/run_docker.sh
+++ TestSerial/run_docker.sh
@@ -7,5 +7,18 @@ echo $hash
 
 ports=$(python3 -m serial.tools.list_ports)
 echo $ports
+if [[ "$ports" == *"no ports found" ]]; then
+  devices = ""
+  echo "No devices: $devices"
+else
+    for device in $ports
+    do
+        echo $device
+    done
+fi
+
+
+#if ports devices = a string of properly formatted devices
+
 
 #docker run --device=/dev/ttyACM0 --device=/dev/ttyACM1 --device=/dev/ttyUSB0 --device=/dev/ttyUSB1 ecy14mhfh/testserial:$hasg
