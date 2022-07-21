a7220fb8ef356a4db7df458ca88cdc00ef287b44
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-21 08:55:07 -0400

    Port list and dockerfile serial ports
    #2  #3 Filled in the list of ports and also made  the it go through the list and look at each port. I also completed the the run command to look at ACM0-1 and USB0-1.

--- TestSerial/TestSerial.py
+++ TestSerial/TestSerial.py
@@ -4,19 +4,30 @@ import serial
 import time
 
 # ports for seiral communication: 
-PORT_NAMES = []
+PORT_NAMES = ['ttyACM0','ttyACM1','ttyUSB0','ttyUSB1']
 
 PORT_NAME = '/dev/ttyUSB0'
 # check which port is being used in pio device monitor V
 
-with  serial.Serial( PORT_NAME,  9600, timeout=1) as ser:
-    print(f"Start Serial: name: {ser.name}.  ")
-    time.sleep(1)
-    for i in range(50):
-        print(f"line: {i}")
-        #if(ser.in_waiting > 0):
-        line = ser.readline()
-        print(line.decode('utf-8'))
+for pName in PORT_NAMES:
+
+    with  serial.Serial( pName,  9600, timeout=1) as ser:
+        print(f"Start Serial: name: {ser.name}.  ")
+        time.sleep(1)
+        for i in range(50):
+            print(f"line: {i}")
+            #if(ser.in_waiting > 0):
+            line = ser.readline()
+            print(line.decode('utf-8'))
+
+# with  serial.Serial( PORT_NAME,  9600, timeout=1) as ser:
+#     print(f"Start Serial: name: {ser.name}.  ")
+#     time.sleep(1)
+#     for i in range(50):
+#         print(f"line: {i}")
+#         #if(ser.in_waiting > 0):
+#         line = ser.readline()
+#         print(line.decode('utf-8'))
 
 ser.close()
 print("Stop Serial: ")
