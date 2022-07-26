718f9cc7ade23d6cab7b39b91fbf8c899d34626e
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-26 11:28:53 -0400

    execute

--- pihattest/docker_pht_run.sh
+++ pihattest/docker_pht_run.sh
@@ -4,4 +4,7 @@ hash_pht=$(git rev-parse --short HEAD)
 
 echo $hash_pht
 
+# this might need to be run privileged
+echo "docker run ecy14mhfh/pihattest:$hash_pht"
+
 docker run ecy14mhfh/pihattest:$hash_pht
