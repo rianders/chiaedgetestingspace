2e1f74cfc687e8d1fd949e37733ec401be4a4b55
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-07-27 16:46:49 -0400

    edge

--- pihattest/docker_pht_run.sh
+++ pihattest/docker_pht_run.sh
@@ -7,5 +7,5 @@ echo $hash_pht
 # this might need to be run privileged
 echo "docker run ecy14mhfh/pihattest:$hash_pht"
 
-docker run -it --device /dev/mem --device /dev/gpiomem ecy14mhfh/pihattest:$hash_pht
+docker run -it --device /dev/mem --device /dev/gpiomem -v /sys/class/gpio:/sys/class/gpio ecy14mhfh/pihattest:$hash_pht
 #docker run -it --privileged ecy14mhfh/pihattest:$hash_pht
