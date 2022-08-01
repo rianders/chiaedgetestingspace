550ff1e22ec826d770311156cd634ec1a81a7ba0
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-01 11:53:48 -0400

    typo fix

--- remade_py/ts/tsnb_builddocker.sh
+++ remade_py/ts/tsnb_builddocker.sh
@@ -2,5 +2,5 @@
 
 hash_tsnb=$(git rev-parse --short HEAD)
 docker build --platform linux/arm64 -t ecy14mhfh/testserialnb:$hash_tsnb -f Dockerfile .
-docker push ecy14mhfh/testserialnb:$hash_ts
+docker push ecy14mhfh/testserialnb:$hash_tsnb
 echo $hash_tsnb
