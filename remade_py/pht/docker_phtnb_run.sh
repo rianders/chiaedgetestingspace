a36e426cf9d82ee5d00d9902611050c156f329ef
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-02 09:27:59 -0400

    pihattest notebook

---
+++ remade_py/pht/docker_phtnb_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hash_pht=$(git rev-parse --short HEAD)
+
+echo $hash_pht
+
+# this might need to be run privileged
+echo "docker run ecy14mhfh/pihattestnb:$hash_pht"
+
+docker run -it --device /dev/mem --device /dev/gpiomem -v /sys/class/gpio:/sys/class/gpio ecy14mhfh/pihattestnb:$hash_pht
+#docker run -it --privileged ecy14mhfh/pihattestnb:$hash_pht
---
+++ remade_py/pht/nbook_phtnb_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "run the pihattest script"
+#python3 pihattest.ipynb
+ipython pihattest.ipynb
+#python3 pihattest.py
