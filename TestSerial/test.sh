1e9ebcf17f2417f7b6c86b220e61e8c3dbad84b9
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-04 13:09:40 -0400

    pio

---
+++ TestSerial/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "run the pihattest script"
+echo "Which python:  $(which python)"
+echo "Python version: $(python --version)"
+
+echo "Which python3:  $(which python3)"
+echo "Python version: $(python3 --version)"
+
--- pihattest/python_pht_run.sh
+++ pihattest/python_pht_run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "run the pihattest script"
+echo "Which python:  $(which python)"
+echo "Which python3:  $(which python3)"
+
+
+
 #python3 pihattest.ipynb
 #strace python3 pihattest.py
 python3 pihattest.py
+echo "Done with  the pihattest script"
+
