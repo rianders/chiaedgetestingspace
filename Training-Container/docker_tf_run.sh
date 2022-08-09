648d93bfb4607331067b3ad6b21c91ef48dcac24
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-09 10:46:57 -0400

    Create docker_tf_run.sh

---
+++ Training-Container/docker_tf_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hash=$(git rev-parse --short HEAD)
+
+echo $hash
+
+# this might need to be run privileged
+echo "docker run trainingcontainer:$hash"
+
+docker run -it --rm -v $(realpath ~/notebooks) -p 8888:8888 ecy14mhfh/trainingcontainer:$hash
