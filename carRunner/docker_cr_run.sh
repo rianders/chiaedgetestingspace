6c20f360d31d3bf63aa88a8871b6e7b52e1d448c
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-10 13:29:18 -0400

    Update docker_cr_run.sh

--- carRunner/docker_cr_run.sh
+++ carRunner/docker_cr_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "docker run -it --device /dev/mem --device /dev/gpiomem -v /sys/class/gpio:/sys/class/gpio ecy14mhfh/carrunner:$hash_pht"
-docker run -it --device /dev/mem --device /dev/gpiomem -v /sys/class/gpio:/sys/class/gpio ecy14mhfh/carrunner:$hash_pht
+echo "docker run -it  ecy14mhfh/carrunner:$hash_pht"
+docker run -it  ecy14mhfh/carrunner:$hash_pht
