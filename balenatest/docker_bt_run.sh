1d1796eda37b28a5208f7d5da95c4116f779734d
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-16 12:01:01 -0400

    interactive

--- balenatest/docker_bt_run.sh
+++ balenatest/docker_bt_run.sh
@@ -4,4 +4,4 @@ hash_tsnb=$(git rev-parse --short HEAD)
 
 echo $hash_tsnb
 
-docker run -p 8888:8888 --privileged -t ecy14mhfh/balenatest:$hash_tsnb
+docker run -p 8888:8888 --privileged -it ecy14mhfh/balenatest:$hash_tsnb /bin/bash
