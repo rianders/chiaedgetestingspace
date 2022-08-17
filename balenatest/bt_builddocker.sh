3f3fbb8d7732bc79d79626d9ee151fd76c251229
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-17 15:17:56 -0400

    Update bt_builddocker.sh

--- balenatest/bt_builddocker.sh
+++ balenatest/bt_builddocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hash_tsnb=$(git rev-parse --short HEAD)
-docker build --platform linux/arm32 -t ecy14mhfh/balenatest:$hash_tsnb -f Dockerfile .
+docker build -t ecy14mhfh/balenatest:$hash_tsnb -f Dockerfile .
 #docker build --platform linux/arm64 -t ecy14mhfh/balenatest:$hash_tsnb -f Dockerfile .
 docker push ecy14mhfh/balenatest:$hash_tsnb
 echo $hash_tsnb
