ac12c4b9474d03e1052a55de1c06f9676d9b62ac
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-24 10:32:33 -0400

    Update piCameraTwo_image.py

--- balenatest/piCameraTwo_image.py
+++ balenatest/piCameraTwo_image.py
@@ -1,9 +1,3 @@
-from picamera2 import Picamera2, Preview
-import time
+from picamera2 import Picamera2
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.DRM)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+picam2.start_and_capture_file("test.jpg")
