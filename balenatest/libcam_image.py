b536f2e981c1da43c80b1fed5b7e1f73f15951ba
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-25 11:48:33 -0400

    libcamera os

---
+++ balenatest/libcam_image.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+# Get date and separate day and time
+date = subprocess.getoutput("date +%F")
+time = subprocess.getoutput("date +%H:%M")
+
+out = os.system("libcamera-jpg -o test.jpg")
+os.system("echo '" + str(out) + "' >> test.txt")
