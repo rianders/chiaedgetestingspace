9fd8ee13ebc539ddc6131ceef06a082a86ef1990
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-29 15:16:17 -0400

    Update docker_bt_run.sh

--- balenatest/docker_bt_run.sh
+++ balenatest/docker_bt_run.sh
@@ -4,22 +4,5 @@ hash_tsnb=$(git rev-parse --short HEAD)
 
 echo $hash_tsnb
 
-DEVICES=""
-
-ports=$(python3 -m serial.tools.list_ports)
-echo "Port Results: $ports"
-if [[ "$ports" == *"no ports found" ]]; then
-  devices = ""
-  echo "No devices: $devices"
-else
-    for device in $ports
-    do
-        echo "--device=$device "
-        DEVICES+="--device=$device "
-    done
-fi
-echo  "$DEVICES"
-
-
-docker run $DEVICES  --privileged -it ecy14mhfh/balenatest:$hash_tsnb #/bin/bash
+docker run -d --name test -v balenatest:/app --privileged -it ecy14mhfh/balenatest:$hash_tsnb #/bin/bash
 #docker run -p 8888:8888 --privileged -it ecy14mhfh/balenatest:$hash_tsnb /bin/bash
