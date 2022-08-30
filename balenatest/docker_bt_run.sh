399f50520e597c5ea3dd573d0096a9caef77953f
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-30 13:13:54 -0400

    Update docker_bt_run.sh

--- balenatest/docker_bt_run.sh
+++ balenatest/docker_bt_run.sh
@@ -6,7 +6,7 @@ echo $hash_tsnb
 
 #docker run -d --name BalImgCon  --mount source=balImage,target=/app/balimages --privileged -it ecy14mhfh/balenatest:$hash_tsnb #/bin/bash
 
-docker run -d --name BalImgCon --rm -v /vartmp:/host:rw ecy14mhfh/balenatest:$hash_tsnb \
+docker run -d --name BalImgCon --rm -v /var/tmp:/host:rw ecy14mhfh/balenatest:$hash_tsnb \
 --volumes-from BalImgCon cp -r /var/ww/html /host/BalImgCon_files
 
 
