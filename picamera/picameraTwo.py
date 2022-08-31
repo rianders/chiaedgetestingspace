36ead41a760ef59041e8d1ae94ed1523fcf9230f
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-31 14:13:42 -0400

    Update picameraTwo.py

--- picamera/picameraTwo.py
+++ picamera/picameraTwo.py
@@ -1,15 +1,3 @@
-from qt_gl_preview import *
-from picamera2 import *
-import time
-
+from picamera2 import Picamera2
 picam2 = Picamera2()
-preview = QtGlPreview(picam2)
-
-preview_config = picam2.preview_configuration()
-capture_config = picam2.still_configuration()
-picam2.configure(preview_config)
-
-picam2.start()
-time.sleep(2)
-
-picam2.switch_mode_and_capture_file(capture_config, "capture.jpg")
+picam2.start_and_capture_file("test.jpg")
