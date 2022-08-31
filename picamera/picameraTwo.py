71d8db5cefcbfe14976c216a8524d8c65e9e81bc
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-31 13:52:25 -0400

    picamera

--- picamera/picameraTwo.py
+++ picamera/picameraTwo.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
-# Capture a JPEG while still running in the preview mode. When you
-# capture to a file, the return value is the metadata for that image.
+# Capture a full resolution image to memory rather than to a file.
 
 import time
 
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.configure(preview_config)
-
 picam2.start_preview(Preview.QTGL)
+preview_config = picam2.create_preview_configuration()
+capture_config = picam2.create_still_configuration()
 
+picam2.configure(preview_config)
 picam2.start()
 time.sleep(2)
 
-metadata = picam2.capture_file("test.jpg")
-print(metadata)
+image = picam2.switch_mode_and_capture_image(capture_config)
+image.show()
+
+
+time.sleep(5)
 
 picam2.close()
