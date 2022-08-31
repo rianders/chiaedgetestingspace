58512af85a73a9c7213440852d8115b50207c906
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-31 11:31:10 -0400

    Update docker_pct_run.sh

--- picamera/docker_pct_run.sh
+++ picamera/docker_pct_run.sh
@@ -5,7 +5,8 @@ hash_tsnb=$(git rev-parse --short HEAD)
 echo $hash_tsnb
 
 
+docker run --rm --privileged -it  ecy14mhfh/picametwo:$hash_tsnb
 
-docker run --rm --privileged -it -v /tmp/foo:/app/picamTwo_img ecy14mhfh/picametwo:$hash_tsnb sh
+#docker run --rm --privileged -it -v /tmp/foo:/app/picamTwo_img ecy14mhfh/picametwo:$hash_tsnb sh
 #ls /app/picamTwo_img/
 
