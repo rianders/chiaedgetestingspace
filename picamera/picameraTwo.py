d4460933a18df2e508cc3a049efefa3b2083c72d
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-31 14:05:57 -0400

    Update picameraTwo.py

--- picamera/picameraTwo.py
+++ picamera/picameraTwo.py
@@ -1,24 +1,15 @@
-#!/usr/bin/python3
-
-# Capture a full resolution image to memory rather than to a file.
-
+from qt_gl_preview import *
+from picamera2 import *
 import time
 
-from picamera2 import Picamera2, Preview
-
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-preview_config = picam2.create_preview_configuration()
-capture_config = picam2.create_still_configuration()
+preview = QtGlPreview(picam2)
 
+preview_config = picam2.preview_configuration()
+capture_config = picam2.still_configuration()
 picam2.configure(preview_config)
+
 picam2.start()
 time.sleep(2)
 
-image = picam2.switch_mode_and_capture_image(capture_config)
-image.show()
-
-
-time.sleep(5)
-
-picam2.close()
+picam2.switch_mode_and_capture_file(capture_config, "capture.jpg")
