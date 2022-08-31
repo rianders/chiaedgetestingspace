9134a444763f5c54dd2b174da8a25566960115d3
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-08-31 09:26:13 -0400

    Update libcam_image.py

--- balenatest/libcam_image.py
+++ balenatest/libcam_image.py
@@ -4,19 +4,19 @@ import os
 import subprocess
 
 # Get date and separate day and time
-#date = subprocess.getoutput("date +%F")
-#time = subprocess.getoutput("date +%H:%M")
+date = subprocess.getoutput("date +%F")
+time = subprocess.getoutput("date +%H:%M")
 
 print("Start libcamera")
 # Take picture
 #cmd = "libcamera-jpeg -o test.jpg"
 
-cmd = os.system("cd app/balena")
+# cmd = os.system("cd app/balena")
 
-cmd = "ls"
-out = os.system(cmd)
-cmd = "libcamera-jpeg -o test.jpg"
-out = os.system(cmd)
+# cmd = "ls"
+# out = os.system(cmd)
+# cmd = "libcamera-jpeg -o test.jpg"
+# out = os.system(cmd)
 #out = os.system("libcamera-jpeg -o test.jpg")
-#out = os.system("libcamera-still -o "+date+"/"+time+".jpg -n")
+out = os.system("libcamera-still -o "+date+"/"+time+".jpg -n")
 print("End libcamera")
