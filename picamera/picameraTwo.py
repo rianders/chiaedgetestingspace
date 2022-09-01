3c297ec8e760b67e6d17da9f323eb608d4cda4a5
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-09-01 12:15:59 -0400

    Update picameraTwo.py

--- picamera/picameraTwo.py
+++ picamera/picameraTwo.py
@@ -1,13 +1,13 @@
 import libcamera
 libcamera.ColorSpace.Sycc = libcamera.ColorSpace.Jpeg
-import picamera2 
+import picamera2 as Picamera2
 
-from picamera2 import Picamera2, Preview
+# from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
+# camera_config = picam2.create_preview_configuration()
+# picam2.configure(camera_config)
+# picam2.start_preview(Preview.QTGL)
 picam2.start()
 time.sleep(2)
 picam2.capture_file("test.jpg")
