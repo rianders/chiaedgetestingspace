85f5702908ef463bf0603fa449715c77100cae05
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-09-01 14:41:40 -0400

    install

--- picamera/picameraTwo.py
+++ picamera/picameraTwo.py
@@ -1,8 +1,7 @@
 
 import libcamera
 #libcamera.ColorSpace.Sycc = libcamera.ColorSpace.Jpeg
-
-import picamera2 # as Picamera2
+import picamera2 as Picamera2
 
 #Above is a work around to get picamera to work. If libcamera or picamera2 is 
 #updated this will also need to be updated.
@@ -10,16 +9,13 @@ import picamera2 # as Picamera2
 import time
 
 
-picam2 = picamera2()
+picam2 = Picamera2()
 
 picam2.start()
 time.sleep(2)
 
-
 fname = (time.strftime("%y-%b-%d_%H:%M"))
-
 variable = fname + ".jpg"
-
 picam2.capture_file(variable)
 #picam2.capture_file("test.jpg")
 
