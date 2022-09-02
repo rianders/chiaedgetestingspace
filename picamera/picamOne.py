d10f2002c07cb63be8a6ffd33acfcb532d182229
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-09-02 13:32:46 -0400

    Delete picamOne.py

--- picamera/picamOne.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from picamera2 import PiCamera2
-
-camera = PiCamera2()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture('foo.jpg')
