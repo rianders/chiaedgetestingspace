4717745107ef63819c71857f473756de37f47244
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-09-06 10:31:01 -0400

    Update picameraTwo_stream.py

--- picamera/picameraTwo_stream.py
+++ picamera/picameraTwo_stream.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python3
-
-import socket
 import time
 
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
+from picamera2.encoders import JpegEncoder
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration({"size": (1280, 720)})
+video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
 picam2.configure(video_config)
-encoder = H264Encoder(1000000)
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(("REMOTEIP", 10001))
-    stream = sock.makefile("wb")
-    picam2.start_recording(encoder, FileOutput(stream))
-    time.sleep(20)
-    picam2.stop_recording()
+picam2.start_preview()
+encoder = JpegEncoder(q=70)
+
+picam2.start_recording(encoder, 'test.mjpeg')
+time.sleep(10)
+picam2.stop_recording()
+
+
+
+
+
+
+# #!/usr/bin/python3
+
+# import socket
+# import time
+
+# from picamera2 import Picamera2
+# from picamera2.encoders import H264Encoder
+# from picamera2.outputs import FileOutput
+
+# picam2 = Picamera2()
+# video_config = picam2.create_video_configuration({"size": (1280, 720)})
+# picam2.configure(video_config)
+# encoder = H264Encoder(1000000)
+
+# with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+#     sock.connect(("REMOTEIP", 10001))
+#     stream = sock.makefile("wb")
+#     picam2.start_recording(encoder, FileOutput(stream))
+#     time.sleep(20)
+#     picam2.stop_recording()
