026e164b3c4a309a451b868f25b1e69a7cfd3a76
HornOfSaltff <98132867+HornOfSaltff@users.noreply.github.com>
2022-09-06 14:34:18 -0400

    change mjpeg name.

--- picamera/picameraTwo_mjpg.py
+++ picamera/picameraTwo_mjpg.py
@@ -4,6 +4,9 @@ import time
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 
+fname = (time.strftime("%y-%b-%d_%H:%M"))
+variable = fname + ".mjpeg"
+
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
 picam2.configure(video_config)
@@ -11,7 +14,7 @@ picam2.configure(video_config)
 picam2.start_preview()
 encoder = JpegEncoder(q=70)
 
-picam2.start_recording(encoder, 'test.mjpeg')
+picam2.start_recording(encoder, variable)
 print("start")
 time.sleep(30)
 picam2.stop_recording()
